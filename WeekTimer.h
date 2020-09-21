9375d11323db1dd95a8f9648a20674a935bed1eb
banchev2001 <68676929+banchev2001@users.noreply.github.com>
2020-09-21 16:18:18 +0300

    Update WeekTimer.h

--- WeekTimer.h
+++ WeekTimer.h
@@ -15,29 +15,35 @@ class WeekTimer
 {
 public:
     
-    /*************************** Vars *****************************************/
-    int8_t WeekPlan; //Contain work days in week 1 for On 0 Off LSB don't care MSB is Sunday
-    int8_t OnHour; // Contain Hour to on
-    int8_t OnMinutes; //Contain Minutes to on
-    int8_t OffHour; //Contain Hour to Off
-    int8_t OffMinutes; //Contain Minutes to Off
-	
-	
-    /************************* Input Methods *********************************/
+	/*************************** Vars *****************************************/
+	int8_t WeekPlan; //Contain work days in week 1 for On 0 Off LSB don't care MSB is Sunday
+        int8_t OnHour; // Contain Hour to on
+        int8_t OnMinutes; //Contain Minutes to on
+        int8_t OffHour; //Contain Hour to Off
+        int8_t OffMinutes; //Contain Minutes to Off
+		
+	/************************* Input Methods *********************************/
                                                                    //SSFTWTM-  S-F-W-M
-    void EnableWD(int8_t DOW); //Turn On Day of week that work timer 11111110 01010101
-    void DisableWD(int8_t DOW); //Turn Off Day of week that work timer
-    void SetWeekPlanStr(char* WeekPlanStr); //Set week plan as a string like this "SS--WTM"
+        void EnableWD(int8_t DOW); //Turn On Day of week that work timer 11111110 01010101
+        void DisableWD(int8_t DOW); //Turn Off Day of week that work timer
+	void SetWeekPlanStr(char* WeekPlanStr); //Set week plan as a string like this "SS--WTM"
     
+	void SetOnTime(int8_t Hr, int8_t Min);  //Set On  time with validating
+	void SetOffTime(int8_t Hr, int8_t Min); //Set Off time with validating
+	
+	void ValOnTime(int8_t Hr, int8_t Min);  // Validate time On Time
+	void ValOffTime(int8_t Hr, int8_t Min); // Validate Off Time
+	
     /************************* Output Methods ********************************/
-    char *GetWeekPlanStr(); // Convert Byte that contain work days in week "WDInWeek" to a string
+	char *GetWeekPlanStr(); // Convert Byte that contain work days in week "WDInWeek" to a string
     int8_t Check (int8_t DOW, int8_t Hour, int8_t Minutes); //Checking When timer to ON and OFF
 	
 private:
 
-     int16_t _CombTime (int8_t Hr, int8_t Min);//Combine hour and minutes to one variable 
-						//nesessery for Check method
+	int16_t _CombTime (int8_t Hr, int8_t Min);//Combine hour and minutes to one variable 
+								//nesessery for Check, ValOnTime and ValOffTime methods
  
 };
 
 #endif
+
